e96844f5975d3b413582c4e3d46199ffc5102e68
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2021-03-27 14:57:16 +0200

    Definition of a dummy problem works.

--- src/views/ProblemDefinition.tsx
+++ src/views/ProblemDefinition.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
 import { Tokens } from "../types/AppTypes";
 
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Form } from "react-bootstrap";
+import { TupleType } from "typescript";
 
 interface ProblemDefinitionProps {
   isLoggedIn: boolean;
@@ -11,16 +15,99 @@ interface ProblemDefinitionProps {
   apiUrl: string;
 }
 
+interface ProblemData {
+  problem_type: string;
+  name: string;
+  objective_functions: string[];
+  objective_names: string[];
+  variables: string[];
+  variable_initial_values: number[];
+  variable_bounds: number[][];
+  variable_names: string[];
+  ideal: number[];
+  nadir: number[];
+  minimize: number[];
+}
+
+interface ProblemNameAndType {
+  name: string;
+  type: string;
+}
+
 function ProblemDefinition({
   isLoggedIn,
   loggedAs,
   tokens,
   apiUrl,
 }: ProblemDefinitionProps) {
+  const [problemDefined, SetProblemDefined] = useState<boolean>(false);
+  const [
+    problemNameAndType,
+    SetProblemNameAndType,
+  ] = useState<ProblemNameAndType>({ name: "", type: "" });
+  const { register, handleSubmit, errors } = useForm<ProblemData>();
+
+  const dummyProblem: ProblemData = {
+    problem_type: "Analytical",
+    name: "TestProblem",
+    objective_functions: ["2*x+y", "x+2*y/z", "x+y+z"],
+    objective_names: ["profit", "loss", "impact"],
+    variables: ["x", "y", "z"],
+    variable_initial_values: [5, 2, 3],
+    variable_bounds: [
+      [-5, 5],
+      [-15, 15],
+      [-20, 20],
+    ],
+    variable_names: ["speed", "luck", "dex"],
+    ideal: [10, 20, 30],
+    nadir: [-10, -20, -30],
+    minimize: [1, -1, 1],
+  };
+
+  const onSubmit = async (data: ProblemData) => {
+    console.log(JSON.stringify(dummyProblem));
+
+    try {
+      const res = await fetch(`${apiUrl}/problem/create`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${tokens.access}`,
+        },
+        body: JSON.stringify(dummyProblem),
+      });
+      if (res.status == 201) {
+        //status ok
+        const body = await res.json();
+        SetProblemNameAndType({ name: body.name, type: body.problem_type });
+        SetProblemDefined(true);
+      }
+    } catch (e) {
+      console.log("not ok");
+      console.log(e);
+      // do nothing
+    }
+  };
+
   return (
-    <Container>
-      <Button>Define test problem</Button>
-    </Container>
+    <>
+      {!problemDefined && (
+        <Container>
+          <Form action="" onSubmit={handleSubmit(onSubmit)}>
+            <Button type="submit">Define dummy problem</Button>
+          </Form>
+        </Container>
+      )}
+      {problemDefined && (
+        <Container>
+          <p>{`Problem '${problemNameAndType.name}' of type '${problemNameAndType.type}' successfully defined!`}</p>
+          <Button onClick={() => SetProblemDefined(false)}>
+            Define a new problem
+          </Button>
+        </Container>
+      )}
+    </>
   );
 }
 
