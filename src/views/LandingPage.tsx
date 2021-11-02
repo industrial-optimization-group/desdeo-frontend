347741f1b920ff59e9d34e17dac9ecda2439d9e8
Giovanni Misitano <giovanni.misitano@jyu.fi>
2021-11-02 15:37:53 +0200

    Started working on cards for the landing page.

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
 
 function LandingPage() {
-  return <Container>This is the landing page</Container>;
+  return <Container>
+    <Row className="justify-content-md-left">
+      <Col xs lg="2">
+        <Card className="card" as={Link} to="/problem/create">
+          <Card.Img variant="top" alt="problem-icon" src="desdeo_logo.png" />
+          <Card.Body>
+            <Card.Title>Define a new problem</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs lg="2">
+        <Card className="card" as={Link} to="/problem/explore">
+          <Card.Img variant="top" alt="problem-icon" src="desdeo_logo.png" />
+          <Card.Body >
+            <Card.Title>Explore an existing problem</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+    <Row className="justify-content-md-left">
+      <Col xs lg="2">
+        <Card className="card" as={Link} to="/problem/create">
+          <Card.Img variant="top" alt="problem-icon" src="desdeo_logo.png" />
+          <Card.Body>
+            <Card.Title>Define a new problem</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs lg="2">
+        <Card className="card" as={Link} to="/problem/explore">
+          <Card.Img variant="top" alt="problem-icon" src="desdeo_logo.png" />
+          <Card.Body >
+            <Card.Title>Explore an existing problem</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>;
 }
 
 export default LandingPage;
